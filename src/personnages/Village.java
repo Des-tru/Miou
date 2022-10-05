c97c8eb80d4bfac79e902c489b3435eaea0c7234
senges noa <snn4401a@ens.fsi>
2022-10-05 15:00:21 +0200

    questionH_tp2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,16 +3,13 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
-	private String[] villageois;
+	private Gaulois[] villageois;
 	private int nbVillageois = 0;
 	
 
-	public Village(String nom, Chef chef, String[] villageois, int nbVillageois) {
-		super();
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
-		this.chef = chef;
-		this.villageois = villageois;
-		this.nbVillageois = nbVillageois;
+		villageois = new Gaulois[nbVillageoisMaximum];
 	}
 
 
@@ -21,8 +18,44 @@ public class Village {
 	}
 	
 	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois]=gaulois;
+		nbVillageois++;
+	}
+	
+	
+	public Gaulois trouverHabitant(int num) {
+		return villageois[num];
+	}
+	
+	
+	public void afficherVillageois() {
+		System.out.println("Dans village du chef "+chef.getNom()+" vivent les légendaires gaulois :");
+		for (int i=0 ; i < nbVillageois ; i++) {
+			System.out.println("- "+trouverHabitant(i).getNom());
+		}
+	}
+	
 	public String getNom() {
 		return nom;
 	}
+
+	
+	public static void main(String[] args) {
+		Village irreductible = new Village("Village des Irréductibles",30);
+		Chef Abraracourcix = new Chef("Abraracourcix", 6);
+		irreductible.setChef(Abraracourcix);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		Gaulois obelix = new Gaulois("Obelix", 25);
+		irreductible.ajouterHabitant(asterix);
+		irreductible.ajouterHabitant(obelix);
+		irreductible.afficherVillageois();
+		
+		
+		
+		
+		
+	}
+	
 	
 }
